573b81eda590d94dd828233823b302544054f168
PieStw <piero.stalteri@icloud.com>
2024-12-04 15:02:07 +0100

    creazione form aggiornato

--- src/components/articleList/ArticleList.jsx
+++ src/components/articleList/ArticleList.jsx
@@ -12,8 +12,14 @@ export default function ArticleList({
         {articleList.map((element, index) => (
           <li key={index} className="item">
             <p>Nome articolo: {element.name}</p>
-            <p>Stato: {element.state}</p>
             <p>Autore: {element.author}</p>
+            <p>Img: {element.img}</p>
+            <p>Contenuto: {element.content}</p>
+            <p>Stato: {element.state}</p>
+            <p>Tags:</p>
+            <p>{element.tags[0] && <p>HTML</p>}</p>
+            <p>{element.tags[1] && <p>CSS</p>}</p>
+            <p>{element.tags[2] && <p>JS</p>}</p>
             <button
               className="btn btn-danger"
               onClick={() => {
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -3,33 +3,45 @@ import "./Form.css";
 import ArticleList from "../articleList/ArticleList";
 
 export default function Form() {
-  const [articleName, setArticleName] = useState("");
-  const [articleAuthor, setArticleAuthor] = useState("");
-  const [articleState, setArticleState] = useState("Draft");
+  const articleDefault = {
+    name: "",
+    author: "",
+    state: "Draft",
+    img: "",
+    content: "",
+    tags: [false, false, false],
+  };
+
+  const [article, setArticle] = useState(articleDefault);
   const [articleList, setArticleList] = useState([]);
 
-  function getArticleName(e) {
-    setArticleName(e.target.value);
-  }
-
-  function getArticleAuthor(e) {
-    setArticleAuthor(e.target.value);
-  }
-
-  function getArticleState(e) {
-    setArticleState(e.target.value);
+  function handleFormData(e) {
+    if (e.target.type === "checkbox") {
+      const updatedTags = [...article.tags];
+      if (e.target.name === "HTML") {
+        updatedTags[0] = e.target.checked;
+      } else if (e.target.name === "CSS") {
+        updatedTags[1] = e.target.checked;
+      } else {
+        updatedTags[2] = e.target.checked;
+      }
+      setArticle((article) => ({
+        ...article,
+        tags: updatedTags,
+      }));
+    } else {
+      setArticle((article) => ({
+        ...article,
+        [e.target.name]: e.target.value,
+      }));
+    }
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    const newArticleList = [
-      ...articleList,
-      { name: articleName, state: articleState, author: articleAuthor },
-    ];
+    const newArticleList = [...articleList, article];
     setArticleList(newArticleList);
-    setArticleName("");
-    setArticleAuthor("");
-    setArticleState("Draft");
+    setArticle(articleDefault);
   }
 
   function deleteArticle(index) {
@@ -53,34 +65,99 @@ export default function Form() {
     <>
       <form className="mb-1 form" onSubmit={(e) => handleSubmit(e)}>
         <label className="form-label">Articoli</label>
-        <div>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Inserisci un nuovo articolo"
-            value={articleName}
-            onChange={(e) => getArticleName(e)}
-            required
-          />
-          <select
-            class="form-select"
-            value={articleState}
-            onChange={(e) => getArticleState(e)}
-          >
-            <option value="Draft">Draft</option>
-            <option value="Published">Published </option>
-          </select>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Autore"
-            value={articleAuthor}
-            onChange={(e) => getArticleAuthor(e)}
-            required
-          />
-          <button type="submit" className="btn btn-primary">
-            Inserisci
-          </button>
+        <div className="row">
+          <div className="col-4">
+            <input
+              name="name"
+              type="text"
+              className="form-control "
+              placeholder="Inserisci un nuovo articolo"
+              value={article.name}
+              onChange={(e) => handleFormData(e)}
+              required
+            />
+          </div>
+          <div className="col-4 mb-4">
+            <input
+              name="author"
+              type="text"
+              className="form-control "
+              placeholder="Autore"
+              value={article.author}
+              onChange={(e) => handleFormData(e)}
+              required
+            />
+          </div>
+          <div className="col-4 mb-4">
+            <input
+              name="img"
+              type="text"
+              className="form-control "
+              placeholder="Url immagine: https://picsum.photos/200/300"
+              value={article.img}
+              onChange={(e) => handleFormData(e)}
+              required
+            />
+          </div>
+          <div className="col-4 mb-4">
+            <textarea
+              name="content"
+              type="text-box"
+              className="form-control "
+              placeholder="Contenuto"
+              value={article.content}
+              onChange={(e) => handleFormData(e)}
+              required
+            />
+          </div>
+          <div className="col-4 mb-4">
+            <select
+              name="state"
+              className="form-select"
+              value={article.state}
+              onChange={(e) => handleFormData(e)}
+            >
+              <option value="Draft">Draft</option>
+              <option value="Published">Published </option>
+            </select>
+          </div>
+          <div className="col-4 d-flex justify-content-around mb-4">
+            <div className="me-2">
+              <input
+                type="checkbox"
+                id="HTML"
+                name="HTML"
+                checked={article.tags[0]}
+                onChange={(e) => handleFormData(e)}
+              />
+              <label for="HTML">HTML</label>
+            </div>
+            <div className="me-2">
+              <input
+                type="checkbox"
+                id="CSS"
+                name="CSS"
+                checked={article.tags[1]}
+                onChange={(e) => handleFormData(e)}
+              />
+              <label for="CSS">CSS</label>
+            </div>
+            <div className="me-2">
+              <input
+                type="checkbox"
+                id="JS"
+                name="JS"
+                checked={article.tags[2]}
+                onChange={(e) => handleFormData(e)}
+              />
+              <label for="JS">JS</label>
+            </div>
+          </div>
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary col-12 mt-4 mb-4">
+              Inserisci
+            </button>
+          </div>
         </div>
       </form>
       <div className="articleList">
